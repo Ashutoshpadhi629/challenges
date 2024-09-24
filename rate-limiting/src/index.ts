3e83c6b681ce57be7f3ca1958b6f8947e95f5296
Ashutoshpadhi629 <padhiashutosh106@gmail.com>
2024-09-24 21:24:38 +0530

    index

--- rate-limiting/src/index.ts
+++ rate-limiting/src/index.ts
@@ -3,14 +3,4 @@ import rateLimit from "express-rate-limit";
 
 const app = express();
 
-/* const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  max: 5,
-  message: { error: "Too many requests, please try again later." },
-});
-
-app.get("/status", limiter, (req, res) => {
-  res.status(200).json({ status: "health-task-1" });
-});
- */
 export default app;
